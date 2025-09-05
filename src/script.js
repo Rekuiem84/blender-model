2b70f3c5f1f80030d8ce0182e20236fbe79b7004
Rekuiem84 <morinijoshua@gmail.com>
2025-09-06 01:56:21 +0200

    new model

--- src/script.js
+++ src/script.js
@@ -28,18 +28,19 @@ let mixer = null;
 
 let burger = null;
 
-gltfLoader.load("./models/burger.glb", (gltf) => {
+gltfLoader.load("./models/big-burger.glb", (gltf) => {
 	burger = gltf.scene;
+	burger.scale.setScalar(4);
 	scene.add(burger);
 });
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.camera.far = 15;
